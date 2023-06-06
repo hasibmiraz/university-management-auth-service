f289b99d3ab6d1f3eb5381807b9f834189c33f7a
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-06 20:34:20 +0600

    [Done] Optimieze error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,32 +1,49 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 import { Error } from 'mongoose';
 import config from '../../config';
+import ApiError from '../../errors/ApiErrors';
 import handleValidationError from '../../errors/handleValidationError';
 import { GenericErrorMessage } from '../../interfaces/error';
 
-const globalErrorHandler = (
-  err: Error.ValidationError,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   let statusCode = 500;
   let message = 'Something went wrong.';
   let errorMessages: GenericErrorMessage[] = [];
 
-  if (err?.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(err);
+  if (error?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(error);
     statusCode = simplifiedError.statusCode;
     message = simplifiedError.message;
     errorMessages = simplifiedError.errorMessages;
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode;
+    message = error.message;
+    errorMessages = error.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+  } else if (error instanceof Error) {
+    message = error?.message;
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : [];
+    res.status(statusCode).json({
+      success: false,
+      message,
+      errorMessages,
+      stack: config.env !== 'production' ? error?.stack : undefined,
+    });
   }
 
-  res.status(statusCode).json({
-    success: false,
-    message,
-    errorMessages,
-    stack: config.env !== 'production' ? err?.stack : undefined,
-  });
   next();
 };
 
