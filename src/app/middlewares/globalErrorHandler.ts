8e2d09aaf9ea3e8fe075483fbdc2b4e35acfefda
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-06 18:46:34 +0600

    Simplifying errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,32 @@
 import { NextFunction, Request, Response } from 'express';
+import { Error } from 'mongoose';
+import config from '../../config';
+import handleValidationError from '../../errors/handleValidationError';
+import { GenericErrorMessage } from '../../interfaces/error';
+
 const globalErrorHandler = (
-  err: Error,
+  err: Error.ValidationError,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({ err: err });
+  let statusCode = 500;
+  let message = 'Something went wrong.';
+  let errorMessages: GenericErrorMessage[] = [];
+
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  });
   next();
 };
 
