026da3f17c211e2c700bbdbe47b10b75331b5969
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-07 16:20:57 +0600

    Uncaught exception and unhandled rejection errors are being fixed

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,38 @@
 /* eslint-disable no-console */
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import { app } from './app';
 import config from './config';
 import { errorLogger, logger } from './shared/logger';
 
+process.on('uncaughtException', () => {
+  console.log(`Uncaught exception detected. Closing the server...`);
+  process.exit(1);
+});
+
 async function main() {
+  let server: Server;
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () =>
+    server = app.listen(config.port, () =>
       logger.info(`App listening to port ${config.port}`)
     );
     logger.info(`DB connected successfully`);
   } catch (err: unknown) {
     errorLogger.error(`Failed to connect to db: ${err}`);
   }
+
+  process.on('unhandledRejection', error => {
+    console.log(`Unhandled rejection. Closing the server...`);
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error);
+        process.exit(1);
+      });
+    }
+    process.exit(1);
+  });
 }
 
 main();
