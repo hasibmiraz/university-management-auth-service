d6045a321b0c7120857acb8121314ce6d64db04b
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-07 20:25:07 +0600

    Working on Zod error

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,19 +1,8 @@
 import { RequestHandler } from 'express';
-import { z } from 'zod';
 import { UserService } from './user.service';
 
 export const createUser: RequestHandler = async (req, res, next) => {
   try {
-    const createUserZodSchema = z.object({
-      body: z.object({
-        role: z.string({
-          required_error: `'role' is required`,
-        }),
-        password: z.string().optional(),
-      }),
-    });
-    await createUserZodSchema.parseAsync(req);
-
     const { user } = req.body;
     const result = await UserService.createUserToDB(user);
     res.status(200).json({
@@ -21,8 +10,8 @@ export const createUser: RequestHandler = async (req, res, next) => {
       message: 'User created successfully',
       data: result,
     });
-  } catch (err) {
-    next(err);
+  } catch (error) {
+    next(error);
   }
 };
 
