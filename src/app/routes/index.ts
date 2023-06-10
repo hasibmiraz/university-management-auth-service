2708741ef92330d40e1bc00f1a3cd5199b0fcee1
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-10 21:24:47 +0600

    [Done] Create faculty related apis

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { UserRoutes } from '../modules/User/user.route';
+import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.route';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.route';
 const routes: Router = Router();
 
 const moduleRoutes = [
   { path: '/user', route: UserRoutes },
   { path: '/academic-semesters', route: AcademicSemesterRoutes },
+  { path: '/academic-faculties', route: AcademicFacultyRoutes },
 ];
 
 moduleRoutes.forEach(({ path, route }) => routes.use(path, route));
