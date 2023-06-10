17e3d96100b7c34c1dd0065523e39d1c08ff1784
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-10 20:49:10 +0600

    Update Academic semester complete

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -13,6 +13,10 @@ router.post(
 
 router.get('/get-semester', AcademicSemesterController.getAllSemester);
 router.get('/:id', AcademicSemesterController.getSingleSemester);
-router.patch('/:id', AcademicSemesterController.updateSemester);
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.updateAcademicSemesterZodSchema),
+  AcademicSemesterController.updateSemester
+);
 
 export const AcademicSemesterRoutes = router;
