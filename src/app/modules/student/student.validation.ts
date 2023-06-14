d30e6c92b4f19a6ff2fd6c6dcf294d6b0c6a98e8
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-15 02:15:08 +0600

    [Done] Update student completed

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -3,11 +3,13 @@ import { bloodGroup, gender } from '../student/student.constant';
 
 const updateStudentZodSchema = z.object({
   body: z.object({
-    name: z.object({
-      firstName: z.string().optional(),
-      lastName: z.string().optional(),
-      middleName: z.string().optional(),
-    }),
+    name: z
+      .object({
+        firstName: z.string().optional(),
+        lastName: z.string().optional(),
+        middleName: z.string().optional(),
+      })
+      .optional(),
     gender: z.enum([...gender] as [string, ...string[]]).optional(),
     dateOfBirth: z.string().optional(),
     email: z.string().email().optional(),
@@ -19,21 +21,25 @@ const updateStudentZodSchema = z.object({
     academicSemester: z.string().optional(),
     academicDepartment: z.string().optional(),
     academicFaculty: z.string().optional(),
-    guardian: z.object({
-      fatherName: z.string().optional(),
-      fatherOccupation: z.string().optional(),
-      fatherContactNo: z.string().optional(),
-      motherName: z.string().optional(),
-      motherOccupation: z.string().optional(),
-      motherContactNo: z.string().optional(),
-      address: z.string().optional(),
-    }),
-    localGuardian: z.object({
-      name: z.string().optional(),
-      occupation: z.string().optional(),
-      contactNo: z.string().optional(),
-      address: z.string().optional(),
-    }),
+    guardian: z
+      .object({
+        fatherName: z.string().optional(),
+        fatherOccupation: z.string().optional(),
+        fatherContactNo: z.string().optional(),
+        motherName: z.string().optional(),
+        motherOccupation: z.string().optional(),
+        motherContactNo: z.string().optional(),
+        address: z.string().optional(),
+      })
+      .optional(),
+    localGuardian: z
+      .object({
+        name: z.string().optional(),
+        occupation: z.string().optional(),
+        contactNo: z.string().optional(),
+        address: z.string().optional(),
+      })
+      .optional(),
     profileImage: z.string().optional(),
   }),
 });
