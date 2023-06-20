f11e33df7d9756754760eae852c525e7b36f1600
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-20 21:07:59 +0600

    [Done] Created model, interface for the faculty

---
+++ src/app/modules/faculty/faculty.constant.ts
@@ -0,0 +1,12 @@
+import { IFacultyBloodGroup } from './faculty.interface';
+
+export const facultyBloogGroup: IFacultyBloodGroup[] = [
+  'A+',
+  'A-',
+  'B+',
+  'B-',
+  'AB+',
+  'AB-',
+  'O+',
+  'O-',
+];
