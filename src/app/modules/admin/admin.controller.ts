57220171f7e8133ec9599c25294d95b5935eb447
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-21 01:55:55 +0600

    [Done] Update and delete admin

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -17,6 +17,33 @@ const getSingleAdmin = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const updateAdmin = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updatedData = req.body;
+  const result = await AdminService.updateAdmin(id, updatedData);
+
+  sendResponse<IAdmin>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin updated successfully',
+    data: result,
+  });
+});
+
+const deleteAdmin = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AdminService.deleteAdmin(id);
+
+  sendResponse<IAdmin>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Admin deleted successfully',
+    data: result,
+  });
+});
+
 export const AdminController = {
   getSingleAdmin,
+  updateAdmin,
+  deleteAdmin,
 };
