280712ed6c5e26fa0d5297a9010f1fb4a9b85670
hasibmiraz <hasibmiraz09@gmail.com>
2023-06-22 17:43:26 +0600

    [Done] Authentication and authorization

--- src/enums/user.ts
+++ src/enums/user.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export const enum ENUM_USER_ROLE {
   SUPER_ADMIN = 'super_admin',
-  ADMIN_USER = 'admin',
+  ADMIN = 'admin',
   STUDENT = 'student',
   FACULTY = 'faculty',
 }
